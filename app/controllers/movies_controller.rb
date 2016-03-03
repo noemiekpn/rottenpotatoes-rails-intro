1d7f4bfdce2a55f396ef56e8bf9a44c3dda9e227
noemiekpn <noemiekpn@gmail.com>
2016-03-02 23:38:10 -0600

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,22 @@ class MoviesController < ApplicationController
   end
 
   def index
+  	# Get the sort mode (either title or release_date)
     @sort_mode = params[:sort_by]
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
-    @movies = Movie.all.order("#{@sort_mode}")
+    
+    # Get the list of rating values from db
+    @all_ratings = Movie.ratings_list
+    
+    # Get the list of user selected ratings
+    @checked_ratings = params[:ratings]
+    if !@checked_ratings then @checked_ratings = Hash.new end
+    
+    # Filter the movies according to selection
+    if @checked_ratings.empty?
+      @movies = Movie.all.order("#{@sort_mode}")
+    else
+      @movies = Movie.where(:rating => @checked_ratings.keys).order("#{@sort_mode}")
+    end
   end
 
   def new
