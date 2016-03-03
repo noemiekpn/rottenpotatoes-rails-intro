7f24634999dd73d59307c3dfee0f9c3cad6b5775
noemiekpn <noemiekpn@gmail.com>
2016-03-02 21:17:52 -0600

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort_mode = params[:sort_by]
+    @movies = Movie.all.order("#{@sort_mode}")
   end
 
   def new
